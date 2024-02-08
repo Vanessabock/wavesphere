4d2487c8dea28343f3d5869ef8ea8f241f48932a
vanessabock <vanessabock54@gmail.com>
2024-02-08 06:58:24 +0100

    Update station card to like station

--- frontend/src/components/station-card.tsx
+++ frontend/src/components/station-card.tsx
@@ -4,18 +4,33 @@ import placeholderRadio from "./../assets/radio-placeholder.png"
 import {PauseIcon} from "./pause-icon.tsx";
 import {PlayIcon} from "./play-icon.tsx";
 import {WebsiteLinkIcon} from "./website-link-icon.tsx";
+import {FavouriteIcon} from "./favourite-icon.tsx";
 
 type StationCardProps = {
     station: Station
     isPlaying: boolean
     togglePlayPause: (station: Station) => void
+    isFavourite: boolean;
+    removeFavourite: (id: string) => void;
+    setFavourite: (id: string) => void;
 }
 
-export const StationCard: React.FC<StationCardProps> = ({station, isPlaying, togglePlayPause}) => {
+export const StationCard: React.FC<StationCardProps> = ({
+                                                            station,
+                                                            isPlaying,
+                                                            togglePlayPause,
+                                                            isFavourite,
+                                                            removeFavourite,
+                                                            setFavourite
+                                                        }) => {
 
     const toggleRadio = () => {
         togglePlayPause(station)
     }
+
+    const toggleFavourite = () => {
+        isFavourite ? removeFavourite(station.stationuuid) : setFavourite(station.stationuuid);
+    };
 
     return (
         <div className="flex flex-col justify-center">
@@ -24,10 +39,12 @@ export const StationCard: React.FC<StationCardProps> = ({station, isPlaying, tog
                     <img src={station.favicon || placeholderRadio} alt="RadioImage"
                          className="flex h-7 mr-5 select-none object-contain"/>
                     <p className="pt-1"> {station.name} </p>
-                    <a className="border-transparent cursor-pointer pl-3 pt-2" href={station.homepage} target="_blank"><WebsiteLinkIcon/></a>
+                    <a className="border-transparent cursor-pointer pl-3 pt-2" href={station.homepage}
+                       target="_blank"><WebsiteLinkIcon/></a>
                 </div>
-                <div>
-                    <button className="flex border-transparent" onClick={toggleRadio}>
+                <div className="flex flex-row ">
+                    <button className="border-transparent mr-2" onClick={toggleFavourite}><FavouriteIcon isActive={isFavourite}/></button>
+                    <button className="border-transparent" onClick={toggleRadio}>
                         {isPlaying ? <PauseIcon/> : <PlayIcon/>}
                     </button>
                     {isPlaying && (<audio className="hidden" controls autoPlay>
