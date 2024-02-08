568a59c9f47aad4cbd6160a06dc866b3f36d0c9c
vanessabock <vanessabock54@gmail.com>
2024-02-08 06:59:14 +0100

    Update user to store liked station

--- frontend/src/types/User.ts
+++ frontend/src/types/User.ts
@@ -1,7 +1,8 @@
+import {Station} from "./Station.ts";
+
 export type User = {
     id: string;
     name: string;
     githubId: number;
-    favouriteStations: string[];
-
+    favouriteStations: Station[];
 } | null;
