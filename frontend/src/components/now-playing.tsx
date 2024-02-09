1ec3f1b0ad2f15546eca3adb50d3c278f729b544
vanessabock <vanessabock54@gmail.com>
2024-02-09 11:29:41 +0100

    Refactor

--- frontend/src/components/now-playing.tsx
+++ frontend/src/components/now-playing.tsx
@@ -20,7 +20,7 @@ export const NowPlaying: React.FC<NowPlayingProps> = ({nowPlayingStation, toggle
                 <div className="flex flex-col bottom-0 sticky h-40 w-full bg-[#17233c] text-[#f8f1e6] text-2xl">
                     <div className="flex justify-center m-4">{nowPlayingStation.name}</div>
                     <button className="flex justify-center border-transparent" onClick={toggleRadio}>
-                        {!!nowPlayingStation.stationuuid ? <PauseIcon size={"50px"}/> : <PlayIcon size={"50px"}/>}
+                        {nowPlayingStation.stationuuid ? <PauseIcon size={"50px"}/> : <PlayIcon size={"50px"}/>}
                     </button>
                 </div>)}
         </>
