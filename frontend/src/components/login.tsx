511c829c8c4bde984813fd1235dc7c040a2911ce
vanessabock <vanessabock54@gmail.com>
2024-02-13 10:22:36 +0100

    Change login

--- frontend/src/components/login.tsx
+++ frontend/src/components/login.tsx
@@ -1,16 +1,18 @@
 import githubMark from "./../assets/github-mark.png"
 
 export const Login = () => {
-    const link = import.meta.env.PROD
-        ? "/oauth2/authorization/github"
-        : "http://localhost:8080/oauth2/authorization/github";
+    const login = () => {
+        const host = window.location.host === 'localhost:5173' ? 'http://localhost:8080' : window.location.origin
+
+        window.open(host + '/oauth2/authorization/github', '_self')
+    }
 
 
     return (
         <div className="mx-auto flex flex-1 items-center justify-center pb-20">
-            <button className="flex h-max items-center rounded-lg px-5 py-4 text-xl font-light">
+            <button className="flex h-max items-center rounded-lg px-5 py-4 text-xl font-light" onClick={login}>
                 <img className="mr-3 h-8" src={githubMark} alt="GitHub"/>
-                <a href={link}>Login with GitHub</a>
+                Login with GitHub
             </button>
         </div>
     );
