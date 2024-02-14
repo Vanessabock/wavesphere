c9ed9c52e637e014849e798dd313608706c1e2f9
vanessabock <vanessabock54@gmail.com>
2024-02-14 15:00:59 +0100

    Add volume and mute icons

--- frontend/src/components/now-playing.tsx
+++ frontend/src/components/now-playing.tsx
@@ -2,6 +2,8 @@ import {Station} from "../types/Station.ts";
 import React, {useEffect, useRef, useState} from "react";
 import {PauseIcon} from "./pause-icon.tsx";
 import {PlayIcon} from "./play-icon.tsx";
+import {VolumeIcon} from "./volume-icon.tsx";
+import {MuteIcon} from "./mute-icon.tsx";
 import Slider from '@mui/joy/Slider';
 
 type NowPlayingProps = {
@@ -44,7 +46,8 @@ export const NowPlaying: React.FC<NowPlayingProps> = ({
                 <div className="flex flex-col bottom-0 sticky h-40 w-full bg-[#17233c] text-[#f8f1e6] text-2xl">
                     <div className="flex justify-center m-4">{stationOnNowPlayingScreen.name}</div>
                     <div className="flex flex-row justify-between ml-7 pl-5 w-1/2">
-                        <div className="w-1/3 px-5">
+                        <div className="flex flex-row w-1/2 items-center px-5 gap-4">
+                            {volume === 0 ? <MuteIcon/> : <VolumeIcon/>}
                             <Slider min={0} max={1} step={0.01} defaultValue={0.5} onChange={toggleSlider}/>
                         </div>
                         <button className="flex justify-center border-transparent" onClick={toggleRadio}>
