c705217bd21dd4a28d56f41536e7d72919fc4331
vanessabock <vanessabock54@gmail.com>
2024-02-15 16:09:55 +0100

    Add equals and hashcode method

--- backend/src/main/java/de/vanessabock/backend/radiostation/model/RadioStation.java
+++ backend/src/main/java/de/vanessabock/backend/radiostation/model/RadioStation.java
@@ -1,11 +1,12 @@
-package de.vanessabock.backend.station.model;
+package de.vanessabock.backend.radiostation.model;
+
+import java.util.Objects;
 
 public class RadioStation {
     String stationuuid;
     String name;
     String url;
     String homepage;
-
     String favicon;
 
     public RadioStation(String stationuuid, String name, String url, String homepage, String favicon) {
@@ -45,5 +46,18 @@ public class RadioStation {
 
     public String getFavicon() {
         return favicon;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RadioStation that = (RadioStation) o;
+        return Objects.equals(stationuuid, that.stationuuid) && Objects.equals(name, that.name) && Objects.equals(url, that.url) && Objects.equals(homepage, that.homepage) && Objects.equals(favicon, that.favicon);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(stationuuid, name, url, homepage, favicon);
     }
 }
