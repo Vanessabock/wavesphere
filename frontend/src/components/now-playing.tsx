2a1e60a8fdd35ebb1d9f46fee9122d4981f7700c
vanessabock <vanessabock54@gmail.com>
2024-02-23 11:15:24 +0100

    Add audio source + refactor

--- frontend/src/components/now-playing.tsx
+++ frontend/src/components/now-playing.tsx
@@ -1,60 +1,83 @@
-import {Station} from "../types/Station.ts";
-import React, {useEffect, useRef, useState} from "react";
-import {PauseIcon} from "../icons/pause-icon.tsx";
-import {PlayIcon} from "../icons/play-icon.tsx";
-import {VolumeIcon} from "../icons/volume-icon.tsx";
-import {MuteIcon} from "../icons/mute-icon.tsx";
-import Slider from '@mui/joy/Slider';
+import { Station } from "../types/Station.ts";
+import React, { useEffect, useRef, useState } from "react";
+import { PauseIcon } from "../icons/pause-icon.tsx";
+import { PlayIcon } from "../icons/play-icon.tsx";
+import { VolumeIcon } from "../icons/volume-icon.tsx";
+import { MuteIcon } from "../icons/mute-icon.tsx";
+import Slider from "@mui/joy/Slider";
 
 type NowPlayingProps = {
-    nowPlayingStation: Station;
-    stationOnNowPlayingScreen: Station;
-    togglePlayPause: (station: Station) => void;
+  nowPlayingStation: Station;
+  stationOnNowPlayingScreen: Station;
+  audioSource: string;
+  togglePlayPause: (station: Station) => void;
 };
 
 export const NowPlaying: React.FC<NowPlayingProps> = ({
-                                                          nowPlayingStation,
-                                                          stationOnNowPlayingScreen,
-                                                          togglePlayPause,
-                                                      }) => {
+  nowPlayingStation,
+  stationOnNowPlayingScreen,
+  audioSource,
+  togglePlayPause
+}) => {
+  const audioPlayer = useRef<HTMLAudioElement>(null);
+  const [volume, setVolume] = useState<number>(0.5);
 
-    const audioPlayer = useRef<HTMLAudioElement>(null);
-    const [volume, setVolume] = useState<number>(0.5)
-
-    useEffect(() => {
-        if (audioPlayer.current){
-            audioPlayer.current.volume = volume;
-        }
-    }, [volume]);
-
-    const toggleRadio = () => {
-        if (nowPlayingStation.stationuuid){
-            togglePlayPause(nowPlayingStation)
-        } else {
-            togglePlayPause(stationOnNowPlayingScreen)
-        }
+  useEffect(() => {
+    if (audioPlayer.current) {
+      audioPlayer.current.volume = volume;
     }
+  }, [volume]);
 
-    const toggleSlider = (_event: Event, newValue: number | number[]) => {
-        setVolume(newValue as number);
+  const toggleRadio = () => {
+    if (nowPlayingStation.stationuuid) {
+      togglePlayPause(nowPlayingStation);
+    } else {
+      togglePlayPause(stationOnNowPlayingScreen);
     }
+  };
 
-    return (
-        <>
-            {stationOnNowPlayingScreen.stationuuid && (
-                <div className="flex flex-col bottom-0 sticky h-40 w-full bg-[#17233c] text-[#f8f1e6] text-2xl">
-                    <div className="flex justify-center m-4">{stationOnNowPlayingScreen.name}</div>
-                    <div className="flex flex-row justify-between ml-7 pl-5 w-1/2">
-                        <div className="flex flex-row w-1/2 items-center px-5 gap-4">
-                            {volume === 0 ? <MuteIcon/> : <VolumeIcon/>}
-                            <Slider min={0} max={1} step={0.01} defaultValue={0.5} onChange={toggleSlider}/>
-                        </div>
-                        <button className="flex justify-center border-transparent" onClick={toggleRadio}>
-                            {nowPlayingStation.stationuuid ? <PauseIcon size={"50px"}/> : <PlayIcon size={"50px"}/>}
-                        </button>
-                    </div>
-                    {nowPlayingStation.stationuuid && (<audio className="hidden" ref={audioPlayer} controls autoPlay src={nowPlayingStation.url_resolved}></audio>)}
-                </div>)}
-        </>
-    )
-}
+  const toggleSlider = (_event: Event, newValue: number | number[]) => {
+    setVolume(newValue as number);
+  };
+
+  return (
+    <>
+      {stationOnNowPlayingScreen.stationuuid && (
+        <div className="sticky bottom-0 flex h-40 w-full flex-col bg-[#17233c] text-2xl text-[#f8f1e6]">
+          <div className="m-4 flex justify-center">
+            {stationOnNowPlayingScreen.name}
+          </div>
+          <div className="ml-7 flex w-1/2 flex-row justify-between pl-5">
+            <div className="flex w-1/2 flex-row items-center gap-4 px-5">
+              {volume === 0 ? <MuteIcon /> : <VolumeIcon />}
+              <Slider
+                min={0}
+                max={1}
+                step={0.01}
+                defaultValue={0.5}
+                onChange={toggleSlider}
+              />
+            </div>
+            <button
+              className="flex justify-center border-transparent"
+              onClick={toggleRadio}
+            >
+              {nowPlayingStation.stationuuid ? (
+                <PauseIcon size={"50px"} />
+              ) : (
+                <PlayIcon size={"50px"} />
+              )}
+            </button>
+          </div>
+          <audio
+            className="hidden"
+            ref={audioPlayer}
+            controls
+            autoPlay
+            src={audioSource}
+          ></audio>
+        </div>
+      )}
+    </>
+  );
+};
