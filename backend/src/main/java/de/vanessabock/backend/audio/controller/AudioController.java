f64181f7404e944a5f6ef52b21b4f6ec570bf33e
vanessabock <vanessabock54@gmail.com>
2024-02-27 06:57:43 +0100

    Move addListenedTimeToUser to Service

--- backend/src/main/java/de/vanessabock/backend/audio/controller/AudioController.java
+++ backend/src/main/java/de/vanessabock/backend/audio/controller/AudioController.java
@@ -2,7 +2,6 @@ package de.vanessabock.backend.audio.controller;
 
 import de.vanessabock.backend.radiostation.model.RadioStation;
 import de.vanessabock.backend.radiostation.service.RadioStationService;
-import de.vanessabock.backend.user.model.ListeningStatistic;
 import de.vanessabock.backend.user.model.User;
 import de.vanessabock.backend.user.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
@@ -13,7 +12,6 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -57,28 +55,18 @@ public class AudioController {
             response.setContentType("audio/mpeg");
             response.setHeader("Transfer-Encoding", "chunked");
 
-            // read stream and send in chunkd
+            // read stream and send in chunks
             byte[] buffer = new byte[1024]; // size of the chunk
             int length;
             while ((length = stream.read(buffer)) != -1) {
                 response.getOutputStream().write(buffer, 0, length);
-                response.flushBuffer(); // Wichtig, um den Chunk sofort zu senden
+                response.flushBuffer();
             }
         } catch (IOException | URISyntaxException e) {
 
             double listenedTime = ((System.currentTimeMillis() - startTime) * 0.001);
 
-            if (user != null){
-                if (user.listeningStatistics().stream().anyMatch(s -> s.stationName().equals(radioStation.getName()))){
-                    List<ListeningStatistic> ls = user.listeningStatistics().stream().filter(s -> s.stationName().equals(radioStation.getName())).toList();
-                    user.listeningStatistics().remove(ls.getFirst());
-                    user.listeningStatistics().add(new ListeningStatistic(ls.getFirst().stationName(), ls.getFirst().listenedTime() + listenedTime));
-                } else {
-                    user.listeningStatistics().add(new ListeningStatistic(radioStation.getName(), listenedTime));
-                }
-
-                userService.updateUser(user);
-            }
+            userService.addListenedTimeToUser(user, radioStation.getName(), listenedTime);
         }
         finally {
             // close connection
