6f1fd9dd76e5f12f2128fb8b80d2b3e6aae0c268
vanessabock <vanessabock54@gmail.com>
2024-02-28 07:29:30 +0100

    Refactor

--- backend/src/test/java/de/vanessabock/backend/radiostation/RadioStationControllerIntegrationTest.java
+++ backend/src/test/java/de/vanessabock/backend/radiostation/RadioStationControllerIntegrationTest.java
@@ -9,10 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
