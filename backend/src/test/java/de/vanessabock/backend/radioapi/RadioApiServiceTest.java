4379fedf769bb1339c35ff6beb70dbcbb2281240
vanessabock <vanessabock54@gmail.com>
2024-02-28 06:25:03 +0100

    Remove unused methods

--- backend/src/test/java/de/vanessabock/backend/radioapi/RadioApiServiceTest.java
+++ backend/src/test/java/de/vanessabock/backend/radioapi/RadioApiServiceTest.java
@@ -14,7 +14,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -34,114 +33,6 @@ public class RadioApiServiceTest {
     @AfterAll
     public static void cleanup() throws IOException {
         mockWebServer.shutdown();
-    }
-
-    @DirtiesContext
-    @Test
-    void getStationsOrderedByVotesTest_WhenCount_ThenReturnListOfCountStations() {
-        //GIVEN
-        mockWebServer.enqueue(new MockResponse().addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON).setBody("""
-                [
-                    {
-                        "changeuuid": "599cf698-a707-4d50-8383-e5f342fb9c08",
-                        "stationuuid": "78012206-1aa1-11e9-a80b-52543be04c81",
-                        "serveruuid": "a67a5b07-6189-4442-b5e8-0100f7f56804",
-                        "name": "MANGORADIO",
-                        "url": "http://stream.mangoradio.de/",
-                        "url_resolved": "https://mangoradio.stream.laut.fm/mangoradio?t302=2024-02-06_03-57-04&uuid=fe4f7927-ca77-4f07-a326-bb2781c5f6dd",
-                        "homepage": "https://mangoradio.de/",
-                        "favicon": "",
-                        "tags": "mango,mangoradio,mongo,mongoradio,public radio,webradio",
-                        "country": "Germany",
-                        "countrycode": "DE",
-                        "iso_3166_2": null,
-                        "state": "",
-                        "language": "german",
-                        "languagecodes": "de",
-                        "votes": 673375,
-                        "lastchangetime": "2023-11-04 17:12:56",
-                        "lastchangetime_iso8601": "2023-11-04T17:12:56Z",
-                        "codec": "MP3",
-                        "bitrate": 128,
-                        "hls": 0,
-                        "lastcheckok": 1,
-                        "lastchecktime": "2024-02-06 03:59:07",
-                        "lastchecktime_iso8601": "2024-02-06T03:59:07Z",
-                        "lastcheckoktime": "2024-02-06 03:59:07",
-                        "lastcheckoktime_iso8601": "2024-02-06T03:59:07Z",
-                        "lastlocalchecktime": "2024-02-05 11:06:28",
-                        "lastlocalchecktime_iso8601": "2024-02-05T11:06:28Z",
-                        "clicktimestamp": "2024-02-06 08:20:55",
-                        "clicktimestamp_iso8601": "2024-02-06T08:20:55Z",
-                        "clickcount": 1140,
-                        "clicktrend": -13,
-                        "ssl_error": 0,
-                        "geo_lat": null,
-                        "geo_long": null,
-                        "has_extended_info": false
-                    },
-                    {
-                        "changeuuid": "fd714cf7-8be8-448e-993e-212c0023b1cf",
-                        "stationuuid": "962cc6df-0601-11e8-ae97-52543be04c81",
-                        "serveruuid": "bf38aa73-690a-4da6-8ec7-58d7bd9307d3",
-                        "name": "Dance Wave!",
-                        "url": "https://dancewave.online/dance.mp3",
-                        "url_resolved": "http://stream.dancewave.online:8080/dance.mp3",
-                        "homepage": "https://dancewave.online/",
-                        "favicon": "https://dancewave.online/dw_logo.png",
-                        "tags": "club dance electronic house trance",
-                        "country": "Hungary",
-                        "countrycode": "HU",
-                        "iso_3166_2": null,
-                        "state": "",
-                        "language": "english",
-                        "languagecodes": "en",
-                        "votes": 461199,
-                        "lastchangetime": "2023-12-19 17:32:57",
-                        "lastchangetime_iso8601": "2023-12-19T17:32:57Z",
-                        "codec": "MP3",
-                        "bitrate": 128,
-                        "hls": 0,
-                        "lastcheckok": 1,
-                        "lastchecktime": "2024-02-06 00:21:39",
-                        "lastchecktime_iso8601": "2024-02-06T00:21:39Z",
-                        "lastcheckoktime": "2024-02-06 00:21:39",
-                        "lastcheckoktime_iso8601": "2024-02-06T00:21:39Z",
-                        "lastlocalchecktime": "2024-02-06 00:21:39",
-                        "lastlocalchecktime_iso8601": "2024-02-06T00:21:39Z",
-                        "clicktimestamp": "2024-02-06 08:15:57",
-                        "clicktimestamp_iso8601": "2024-02-06T08:15:57Z",
-                        "clickcount": 1352,
-                        "clicktrend": 23,
-                        "ssl_error": 0,
-                        "geo_lat": null,
-                        "geo_long": null,
-                        "has_extended_info": true
-                    }
-                ]
-                """));
-
-        //WHEN
-        List<RadioStation> actual = radioApiService.getStationsOrderedByVotes("2");
-
-        //THEN
-        assertEquals(List.of(new RadioStation("78012206-1aa1-11e9-a80b-52543be04c81", "MANGORADIO", "https://mangoradio.stream.laut.fm/mangoradio?t302=2024-02-06_03-57-04&uuid=fe4f7927-ca77-4f07-a326-bb2781c5f6dd", "https://mangoradio.de/", "", "mango,mangoradio,mongo,mongoradio,public radio,webradio", "Germany"),
-                new RadioStation("962cc6df-0601-11e8-ae97-52543be04c81", "Dance Wave!", "http://stream.dancewave.online:8080/dance.mp3", "https://dancewave.online/", "https://dancewave.online/dw_logo.png", "club dance electronic house trance", "Hungary")),
-                actual);
-    }
-
-    @DirtiesContext
-    @Test
-    void getStationsOrderedByVotesTest_WhenResponseNull_ThenReturnEmptyList() {
-        //GIVEN
-        mockWebServer.enqueue(new MockResponse().addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON).setBody(""));
-
-        //WHEN
-        List<RadioStation> actual = radioApiService.getStationsOrderedByVotes("2");
-
-        //THEN
-        assertEquals(new ArrayList<>(),
-                actual);
     }
 
     @DirtiesContext
