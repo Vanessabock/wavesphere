795f596694d0057e5a6748f7295a91f7c8aa5ddf
vanessabock <vanessabock54@gmail.com>
2024-02-28 10:23:44 +0100

    Add comments to look for issue

--- backend/src/main/java/de/vanessabock/backend/audio/controller/AudioController.java
+++ backend/src/main/java/de/vanessabock/backend/audio/controller/AudioController.java
@@ -43,34 +43,43 @@ public class AudioController {
             // URL of the radio-stream
             URI url = new URI(radioStation.getUrl_resolved());
             HttpURLConnection connection = (HttpURLConnection) url.toURL().openConnection();
+            System.out.println("46");
             connection.setRequestProperty("Icy-MetaData", "1");
+            System.out.println("48");
 
             // open connection
             InputStream stream = connection.getInputStream();
+            System.out.println("52");
 
             // User register start time
             startTime = System.currentTimeMillis();
+            System.out.println("56");
 
             // activate chunked transfer encoding
             response.setContentType("audio/mpeg");
             response.setHeader("Transfer-Encoding", "chunked");
+            System.out.println("61");
 
             // read stream and send in chunks
             byte[] buffer = new byte[1024]; // size of the chunk
             int length;
             while ((length = stream.read(buffer)) != -1) {
+                System.out.println("67");
                 response.getOutputStream().write(buffer, 0, length);
+                System.out.println("69");
                 response.flushBuffer();
+                System.out.println("71");
             }
         } catch (IOException | URISyntaxException e) {
-
+            e.printStackTrace();
             double listenedTime = ((System.currentTimeMillis() - startTime) * 0.001);
-
             userService.addListenedTimeToUser(user, radioStation.getName(), listenedTime);
         }
         finally {
             // close connection
             response.setStatus(HttpServletResponse.SC_OK);
+            System.out.println("81");
         }
+        System.out.println("83");
     }
 }
